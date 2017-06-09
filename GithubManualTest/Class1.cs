a55c6f2542dcc6dd2da2d3001197a45e42417b03
Joy <joyhaha@hotmail.nl>
2017-06-09 20:42:58 +0200

    Add project files.

---
+++ GithubManualTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubManualTest
+{
+    public class Class1
+    {
+    }
+}
